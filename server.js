ea8796caa81112990557811ed5ddbca3d5df426e
Hifaris <nattapongbe@gmail.com>
2024-10-30 11:22:41 +0700

    first

---
+++ server.js
@@ -0,0 +1,21 @@
+require("dotenv").config()
+const express = require("express")
+const app = express()
+const morgan = require("morgan")
+const cors = require("cors")
+
+const hdlError = require("./middlewares/error")
+const hdlNotFound = require("./middlewares/not-found")
+
+app.use(express.json())
+app.use(morgan('dev'))
+app.use(cors())
+
+app.use(hdlError)
+app.use("*",hdlNotFound)
+
+
+
+
+const port = process.env.PORT || 8000
+app.listen(port,()=>console.log("Server is running on",port))
