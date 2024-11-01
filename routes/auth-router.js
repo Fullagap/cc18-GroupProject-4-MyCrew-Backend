41cb57405c66c25277898d7d8d433cfbfbe3f30b
Oatake <thitiwut.ekwisit@gmail.com>
2024-11-01 17:58:49 +0700

    revise schema

--- routes/auth-router.js
+++ routes/auth-router.js
@@ -1,13 +1,13 @@
 const express = require("express")
 const router = express.Router()
-const {register,login,changePassword,updateProfile, requestChangePassword} = require("../controllers/auth-controller")
+const {login,changePassword, requestChangePassword} = require("../controllers/auth-controller")
 
 
-router.post("/auth/register",register)
+// router.post("/auth/register",register)
 router.post("/auth/login",login)
 router.patch("/auth/request-change-password",requestChangePassword)
 router.patch("/auth/change-password",changePassword)
-router.patch("/auth/update-profile",updateProfile)
+// router.patch("/auth/update-profile",updateProfile)
 
 
 module.exports = router
