113078d248d317ce925ff8e1a30183a98377a015
JP-meowmeow <jp.jirapat.t@gmail.com>
2024-11-06 17:26:09 +0700

    separate controller

--- routes/admin-route.js
+++ routes/admin-route.js
@@ -1,30 +1,85 @@
-const express = require("express")
-const router = express.Router()
-const { register, updateUser, getUserById, getDepartment, getPositionEachDepartment, getEmployeeInDepartment
-    , getEachSuperId, getLeaderEachSupId, getSupIdByDepartment, allEmployees, createDepartment, createPosition,getHeader,getLeadSupId,updateImageProfile,createOfficeSiteLocation } = require("../controllers/admin-controller")
-const { authCheck, adminCheck } = require("../middlewares/authen")
-const {registerValidator} = require("../middlewares/validator")
-const upload = require("../middlewares/upload")
+const express = require("express");
+const router = express.Router();
+const {
+  register,
+  updateUser,
+  getUserById,
+  getDepartment,
+  getPositionEachDepartment,
+  getEmployeeInDepartment,
+  getEachSuperId,
+  getLeaderEachSupId,
+  getSupIdByDepartment,
+  allEmployees,
+  createDepartment,
+  createPosition,
+  getHeader,
+  getLeadSupId,
+  updateImageProfile,
+} = require("../controllers/admin-controller");
+const {
+  createOfficeSiteLocation,
+} = require("../controllers/admin-location-controller");
+const { authCheck, adminCheck } = require("../middlewares/authen");
 
-router.post("/admin/register",authCheck,adminCheck,registerValidator,register)
-router.patch("/admin/update-user/:id",authCheck,adminCheck,updateUser)
-router.get("/admin/user/:id", authCheck, adminCheck, getUserById)
-router.get("/admin/department",authCheck,adminCheck,getDepartment)
-router.get("/admin/All-employees",authCheck,adminCheck,allEmployees)
-router.get("/admin/department-position/:id",authCheck,adminCheck,getPositionEachDepartment)
-router.get("/admin/department-employees/:id",authCheck,adminCheck,getEmployeeInDepartment)
-router.get("/admin/superId-employees/:id", authCheck, adminCheck, getEachSuperId)
-router.get("/admin/leader-superId/:id", authCheck, adminCheck, getLeaderEachSupId)
-router.get("/admin/superId-department/:id", authCheck, adminCheck, getSupIdByDepartment)
-router.post("/admin/create-department", createDepartment)
-router.post("/admin/create-position", createPosition)
-router.get("/admin/leader", getHeader)
-router.post('/admin/site-register',createOfficeSiteLocation)
+const { registerValidator } = require("../middlewares/validator");
+const upload = require("../middlewares/upload");
 
-router.post("/admin/create-department",authCheck,adminCheck, createDepartment)
-router.post("/admin/create-position",authCheck,adminCheck, createPosition)
-router.get("/admin/leader",authCheck,adminCheck, getHeader)
+router.post(
+  "/admin/register",
+  authCheck,
+  adminCheck,
+  registerValidator,
+  register
+);
+router.patch("/admin/update-user/:id", authCheck, adminCheck, updateUser);
+router.get("/admin/user/:id", authCheck, adminCheck, getUserById);
+router.get("/admin/department", authCheck, adminCheck, getDepartment);
+router.get("/admin/All-employees", authCheck, adminCheck, allEmployees);
+router.get(
+  "/admin/department-position/:id",
+  authCheck,
+  adminCheck,
+  getPositionEachDepartment
+);
+router.get(
+  "/admin/department-employees/:id",
+  authCheck,
+  adminCheck,
+  getEmployeeInDepartment
+);
+router.get(
+  "/admin/superId-employees/:id",
+  authCheck,
+  adminCheck,
+  getEachSuperId
+);
+router.get(
+  "/admin/leader-superId/:id",
+  authCheck,
+  adminCheck,
+  getLeaderEachSupId
+);
+router.get(
+  "/admin/superId-department/:id",
+  authCheck,
+  adminCheck,
+  getSupIdByDepartment
+);
+router.post("/admin/create-department", createDepartment);
+router.post("/admin/create-position", createPosition);
+router.get("/admin/leader", getHeader);
+router.post("/admin/site-register", createOfficeSiteLocation);
+
+router.post(
+  "/admin/create-department",
+  authCheck,
+  adminCheck,
+  createDepartment
+);
+router.post("/admin/create-position", authCheck, adminCheck, createPosition);
+router.get("/admin/leader", authCheck, adminCheck, getHeader);
 // router.get("/admin/supId",authCheck,getLeadSupId)
 // router.patch("/admin/update-profile/:id",authCheck,upload.single("file"),updateImageProfile)
 
-module.exports = router
+module.exports = router;
