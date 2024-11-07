6b53828351c8c5d35f256893066850434ca6dcbf
Fullagap <Wichasith.Chaisrithong@gmail.com>
2024-11-07 15:19:10 +0700

    request item

--- controllers/checkRequest-controller.js
+++ controllers/checkRequest-controller.js
@@ -60,3 +60,125 @@ exports.changeComment = async (req,res,next) => {
         next(error)
     }
 }
+
+exports.checkItem = async (req,res,next) => {
+    try {
+        const user = await prisma.Item.findMany({
+        })
+        res.status(200).json(user)
+    } catch (error) {
+        next(error)
+    }
+}
+
+exports.createItem = async (req, res, next) => {
+    try {
+        const { name, cost, categoryId } = req.body;
+
+        const item = await prisma.item.create({
+            data: {
+                itemName: name,  
+                cost: cost,
+                categoryId: categoryId,
+            },
+        });
+
+        res.status(201).json(item); 
+    } catch (error) {
+        next(error)
+    }
+};
+
+exports.updateItem = async (req, res, next) => {
+    try {
+        const { name, cost, categoryId } = req.body;
+        const { id } = req.params;
+
+        const item = await prisma.item.update({
+            where: {
+                id: Number(id),
+            },
+            data: {
+                itemName: name,
+                cost: cost, 
+                categoryId: categoryId,
+            },
+        });
+
+        res.status(200).json(item);
+    } catch (error) {
+        next(error)
+    }
+};
+
+exports.checkRequestItem = async (req,res,next) => {
+    try {
+        const user = await prisma.requestItem.findMany({
+        })
+        res.status(200).json(user)
+    } catch (error) {
+        next(error)
+    }
+}
+
+exports.createRequestItem = async (req, res, next) => {
+    try {
+        const { itemId, userId } = req.body;
+
+        const item = await prisma.requestItem.create({
+            data: {
+                itemId: itemId,
+                userId: userId,
+                status: "REJECT",
+            },
+        });
+
+        res.status(201).json(item); 
+    } catch (error) {
+        next(error)
+    }
+}
+
+exports.changeStatusRequestItem = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const { status } = req.body;
+
+        const item = await prisma.requestItem.update({
+            where: {
+                id: Number(id),
+            },
+            data: {
+                status: status,
+            },
+        });
+
+        res.status(200).json(item);
+    } catch (error) {
+        next(error)
+    }
+}
+
+exports.checkSup = async (req, res, next) => {
+    const { sup_id } = req.params;
+  
+    try {
+      const requests = await prisma.requestItem.findMany({
+        include: {
+          user: true,
+          item: true
+        },
+      });
+  
+      console.log(requests);
+  
+      const filteredRequests = requests.filter((request) => request.user.supId === Number(sup_id));
+      
+      console.log(filteredRequests);
+  
+      res.status(200).json(filteredRequests);
+    } catch (error) {
+      next(error);
+    }
+  };
+  
--- routes/checkRequest-router.js
+++ routes/checkRequest-router.js
@@ -1,10 +1,22 @@
 const express = require("express")
-const {checkRequest,changeStatus,changeComment} = require("../controllers/checkRequest-controller")
+const {checkRequest,changeStatus,changeComment,checkItem,
+    createItem,updateItem,checkRequestItem,createRequestItem,
+    changeStatusRequestItem,checkSup} = require("../controllers/checkRequest-controller")
 const router = express.Router()
 
 
 router.get("/checkRequest/:sup_id",checkRequest )
 router.patch("/changeStatus/:id",changeStatus )
 router.patch("/changeComment/:id",changeComment )
+
+router.get("/checkItem",checkItem)
+router.post("/createItem",createItem)
+router.patch("/updateItem/:id",updateItem)
+
+router.get("/checkRequestItem",checkRequestItem)
+router.post("/createRequestItem",createRequestItem)
+router.patch("/changeStatusRequestItem/:id",changeStatusRequestItem)
+router.get("/checkItemSup/:sup_id",checkSup)
+
 
 module.exports = router
