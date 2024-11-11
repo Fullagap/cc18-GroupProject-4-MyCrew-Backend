57c8e1f9640722cfff5f510e9bb1ad8b302c25b5
JP-meowmeow <jp.jirapat.t@gmail.com>
2024-11-11 15:58:25 +0700

    add edit/delete attendance location feature

--- routes/admin-location-route.js
+++ routes/admin-location-route.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const {  createOfficeSiteLocation} = require("../controllers/admin-location-controller");
+const {  createOfficeSiteLocation,editOfficeSiteLocation,deleteOfficeSiteLocation} = require("../controllers/admin-location-controller");
 const { authCheck, adminCheck } = require("../middlewares/authen");
 
 router.post("/admin/site-register",authCheck,adminCheck,createOfficeSiteLocation);
+router.post("/admin/site-edit",authCheck,adminCheck,editOfficeSiteLocation);
+router.post("/admin/site-delete",authCheck,adminCheck,deleteOfficeSiteLocation);
 
 module.exports = router;
